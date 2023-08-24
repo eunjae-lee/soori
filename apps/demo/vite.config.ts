ac4265a1c97aea3070fd0e3bf1b8dd27b78c6bd9
Eunjae Lee <hey@eunjae.dev>
2023-08-24 10:29:46 +0200

    chore: use resolve.alias for development

--- apps/demo/vite.config.ts
+++ apps/demo/vite.config.ts
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { vite } from 'soori';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), vite()],
+  resolve: {
+    alias:
+      process.env.NODE_ENV === 'development'
+        ? [
+          {
+            find: /^soori$/,
+            replacement: path.resolve(__dirname, '../soori/src/index.ts'),
+          },
+        ]
+        : [],
+  },
 });
--- packages/plugin-json/vite.config.ts
+++ packages/plugin-json/vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vitest/config';
 import dts from 'vite-plugin-dts';
+import path from 'path';
 
 export default defineConfig({
   test: {
     globals: true,
   },
   plugins: [dts()],
+  resolve:
+  {
+    alias: process.env.NODE_ENV === 'development'
+      ? [
+        {
+          find: /^soori$/,
+          replacement: path.resolve(__dirname, '../soori/src/index.ts'),
+        },
+      ] : [],
+  }
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
