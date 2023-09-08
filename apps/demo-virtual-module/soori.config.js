a87546e082d8e28b2d8483bb2c387354e9ed6540
Eunjae Lee <hey@eunjae.dev>
2023-09-08 16:32:28 +0200

    chore: add example for virtual mode

--- apps/demo-virtual-module/soori.config.js
+++ apps/demo-virtual-module/soori.config.js
@@ -1,10 +1,24 @@
 import { defineConfig, definePlugin } from 'soori';
 import json from '@soori/plugin-json';
+import fs from 'fs/promises';
 
 export default defineConfig({
   plugins: [
     definePlugin({
-      outputMode: 'virtual-module',
+      output: {
+        dir: 'node_modules/my-jsons',
+        onBuildStart: async ({ dir }) => {
+          await fs.writeFile(
+            `${dir}/package.json`,
+            `{
+            "name": "my-jsons",
+            "private": true,
+            "type": "module",
+            "main": "index.ts"
+          }`
+          );
+        },
+      },
       ...json({ name: 'json-gen', watch: ['src/jsons/*.json'] }),
     }),
   ],
